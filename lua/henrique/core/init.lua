f179807046a5c355248e4ef25379234840b2ed88
henrique.clemente@outlook.com <henrique.clemente@outlook.com>
2026-03-11 01:42:15 +0000

    make init.lua clipboard config compatible with mac

--- lua/henrique/core/init.lua
+++ lua/henrique/core/init.lua
@@ -1,15 +1,23 @@
 vim.cmd("let g:netrw_liststyle = 3")
 require("henrique.core.options")
 require("henrique.core.keymaps")
-vim.g.clipboard = {
-  name = "WslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
-    ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
-  },
-  cache_enabled = 0,
-}
+
+local uname = vim.loop.os_uname()
+
+local is_wsl = uname.sysname == "Linux"
+  and vim.fn.has("wsl") == 1
+
+if is_wsl then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
+      ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
